aeb2443da16f4a1a5ae5c508c54795de82279e0e
TrisNol <tristan.nolde@yahoo.de>
2022-09-05 10:44:39 +0200

    initial commit

---
+++ setup.ps1
@@ -0,0 +1,20 @@
+# Uninstall Cortana
+Get-AppxPackage -allusers Microsoft.549981C3F5F10 | Remove-AppxPackage
+
+# Ensure Get-ExecutionPolicy is not Restricted
+if((Get-ExecutionPolicy) -eq "Restricted"){
+    write-host("ExecutionPolicy is restricted - set to AllSigned to enable following commands but with a bit of security")
+    Set-ExecutionPolicy AllSigned
+}
+# Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+# Iterate over list of packages hosted in /packages directory and install the tools listed in the nested files
+$files = Get-ChildItem "./packages" -Recurse
+foreach ($file in $files){
+    $file = $file.FullName
+    Write-Host("Processing {$file}")
+    foreach($line in Get-Content $file) {
+        choco install $line
+    }
+}
